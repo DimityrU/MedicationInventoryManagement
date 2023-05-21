a1121d6a5bd14e89b4c7bef2d114a4f78d268b7d
DimityrU <75433184+DimityrU@users.noreply.github.com>
2023-05-21 17:50:38 +0300

    Init commit

---
+++ MedicationInventoryManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MedicationInventoryManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
