be8a022ab1656ac778d6dad11e34646828b69018
DimityrU <75433184+DimityrU@users.noreply.github.com>
2023-06-07 17:39:51 +0300

    Create FE for notifications

---
+++ MedicationInventoryManagement/Controllers/NotificationsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MedicationInventoryManagement.Controllers
+{
+    public class NotificationsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
