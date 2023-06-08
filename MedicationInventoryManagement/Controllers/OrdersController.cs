69bb7d2c6d77fa0e5db01a3ac5fd0054bc6cf3ca
DimityrU <75433184+DimityrU@users.noreply.github.com>
2023-06-08 20:35:49 +0300

    FIx FE desing and sent data to backend

--- MedicationInventoryManagement/Controllers/OrdersController.cs
+++ MedicationInventoryManagement/Controllers/OrdersController.cs
@@ -1,4 +1,5 @@
 ﻿using MedicationInventoryManagement.Models;
+using MedicationInventoryManagement.Services;
 using MedicationInventoryManagement.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,28 +8,67 @@ namespace MedicationInventoryManagement.Controllers
     public class OrdersController : Controller
     {
         private readonly INotificationsService _notificationsService;
+        private readonly IMedicationService _medicationService;
 
 
-        public OrdersController(INotificationsService notificationsService)
+
+        public OrdersController(IMedicationService medicationService, INotificationsService notificationsService)
         {
+            _medicationService = medicationService;
             _notificationsService = notificationsService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            try
+            {
+                if (TempData["ErrorMessage"] != null)
+                {
+                    ModelState.AddModelError("",
+                        TempData["ErrorMessage"]?.ToString() ?? "Error occurred! Please try again!");
+                }
+                else
+                {
+                    var response = await _notificationsService.GetAllNotifications();
+
+
+                    var allMedications = new AllMedicationsViewModel
+                    {
+                        Medications = null,
+                        Notifications = response.Notifications
+                    };
+
+                    return View(allMedications);
+                }
+            }
+            catch (Exception)
+            {
+                ModelState.AddModelError("", "No Medication in the inventory");
+            }
+
+            return View(new AllMedicationsViewModel());
         }
 
-
+        [HttpGet]
         public async Task<IActionResult> Create()
         {
             try
             {
                 var notificationResponse = await _notificationsService.GetAllNotifications();
+                var medications = await _medicationService.GetAllMedications();
+
+                var order = new OrderDTO
+                {
+                    OrderId = null,
+                    OrderName = "",
+                    OrderDate = null,
+                    Status = "",
+                    Medication = medications.Select(m => new OrderMedicationDTO { Medication = new MedicationDTO { MedicationId = m.MedicationId, MedicationName = m.MedicationName }, newQuantity = 0 }).ToList()
+                };
 
                 var response = new OrderViewModel
                 {
-                    Order = null,
+                    Order = order,
                     Notifications = notificationResponse.Notifications
                 };
                 return View(response);
@@ -38,6 +78,15 @@ namespace MedicationInventoryManagement.Controllers
                 TempData["ErrorMessage"] = "Error occurred while redirecting.";
                 return RedirectToAction("Index");
             }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(OrderDTO order)
+        {
+
+
+            return RedirectToAction("Index");
+
         }
     }
 }
