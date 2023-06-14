0137fdb129bf978a0f46c46821ac8899248b1e88
DimityrU <75433184+DimityrU@users.noreply.github.com>
2023-06-15 00:24:54 +0300

    Add filter for medication names

---
+++ MedicationInventoryManagement/wwwroot/js/name-filter.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $("#searchFilter").on("input", function () {
+        var filterValue = $(this).val().toLowerCase();
+
+        $("tbody tr").each(function () {
+            var medicationName = $(this).find("td:first").text().toLowerCase();
+
+            if (medicationName.includes(filterValue)) {
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+        });
+    });
+});
